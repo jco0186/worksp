cd915472b0e697e10f1600892d775a127d76343e
liming <liming@cnjabsco.com>
2013-02-21 10:53:36 +0800

    测试第一

---
+++ j2sedemo/src/org/tangm/text/Test.java
@@ -0,0 +1,14 @@
+package org.tangm.text;
+
+public class Test {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		System.out.println("hello world");
+	}
+
+}
